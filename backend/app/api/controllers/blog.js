c06c23e40f3310e6ba9425b07c16077df2f4178f
MilosM348 <milos.milic001@outlook.com>
2023-04-12 15:32:56 +0200

    fix

--- backend/app/api/controllers/blog.js
+++ backend/app/api/controllers/blog.js
@@ -18,6 +18,10 @@ module.exports = {
       }
     });
   },
+  getAll: async function (req, res, next) {
+    let blogs = await blogModel.find();
+    res.status(200).json({ message: null, data: blogs });
+  },
   getFilter: async function (req, res, next) {
     const filter = req.body;
     let blogs = await blogModel.find(filter);
